7570b473254b243f93763ab728879630bd41d3d3
bally <108769285+ballyalley-o@users.noreply.github.com>
2022-07-18 20:06:11 +1200

    first commit

---
+++ scripts.js
@@ -0,0 +1,44 @@
+class Calculator {
+    constructor(previousOandTextElement, currentOandTextElement) {
+        this.previousOandTextElement = previousOandTextElement
+        this.currentOandTextElement = currentOandTextElement
+    }
+}
+
+
+clear() {
+
+}
+
+delete() {
+
+}
+
+appendNumber(number) {
+
+
+}
+
+chooseOperation(operation) {
+
+}
+
+compute() {
+
+
+}
+
+
+updateDisplay() {
+
+
+}
+
+
+const numberButtons = document.querySelectorAll ('[data-number]')
+const operationButtons = document.querySelectorAll ('[data-operation]')
+const equalsButton = document.querySelectorAll ('[data-equals]')
+const deleteButton = document.querySelectorAll ('[data-delete]')
+const allClearButton = document.querySelectorAll ('[data-allclear]')
+const previousOandTextElement = document.querySelectorAll ('[data-previous-o]')
+const currentOandTextElement = document.querySelectorAll ('[data-current-o]')
